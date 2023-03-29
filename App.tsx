a8c724b4abd4c068444433f36c79d4cadf0cce48
bglgwyng <bglgwyng@gmail.com>
2023-03-29 18:35:39 +0900

    Reproduce memory leak.

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 import {
   Camera,
@@ -17,10 +17,19 @@ import {
 function App(): JSX.Element {
   const device = useCameraDevices().back;
 
-  const frameProcessor = useFrameProcessor(frame => {
-    'worklet';
-    console.info(FrameProcessorPlugins);
+  const [foo, setFoo] = useState({});
+  useEffect(() => {
+    const interval = setInterval(() => setFoo({}), 10);
+    return () => clearInterval(interval);
   }, []);
+
+  const frameProcessor = useFrameProcessor(
+    frame => {
+      'worklet';
+    },
+    // the frame processor is re-created on every change of `foo`.
+    [foo],
+  );
 
   return (
     <View style={{flex: 1}}>
