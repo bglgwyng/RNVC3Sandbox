82e5dbde257ffbef580bafeb29e8a5bec6f23773
bglgwyng <bglgwyng@gmail.com>
2023-05-18 12:55:11 +0900

    Add face tracking.

--- App.tsx
+++ App.tsx
@@ -5,21 +5,80 @@
  * @format
  */
 
-import React from 'react';
-import {View} from 'react-native';
+import React, {useMemo, useState} from 'react';
+import {Animated, StyleSheet, View, useWindowDimensions} from 'react-native';
 import {
   Camera,
+  Frame,
   FrameProcessorPlugins,
   useCameraDevices,
   useFrameProcessor,
 } from 'react-native-vision-camera';
+import {} from 'react-native-worklets';
+import {ContextType} from 'react-native-worklets/src';
 
 function App(): JSX.Element {
-  const device = useCameraDevices().back;
+  const device = useCameraDevices().front;
+
+  const [trackedFaces, setTrackedFaces] = useState<
+    (Face & {
+      animatedXY: Animated.ValueXY;
+      animatdWH: Animated.ValueXY;
+    })[]
+  >([]);
+
+  const windowDimensions = useWindowDimensions();
+
+  const handleScan = useRunInJSCallback(
+    (frameDimensions: Dimensions, faces: Face[]) => {
+      const {adjustPoint: adjustPosition, adjustSize} = ajdustToView(
+        frameDimensions,
+        windowDimensions,
+      );
+
+      setTrackedFaces(prevFaces => {
+        return faces.map(i => {
+          const face = prevFaces.find(j => j.trackingId === i.trackingId);
+
+          const xy = adjustPosition({
+            x: i.bounds.left,
+            y: i.bounds.top,
+          });
+          const wh = adjustSize({
+            x: i.bounds.width,
+            y: i.bounds.height,
+          });
+
+          if (face) {
+            Animated.parallel([
+              Animated.spring(face.animatedXY, {
+                toValue: xy,
+                useNativeDriver: false,
+              }),
+              Animated.spring(face.animatdWH, {
+                toValue: wh,
+                useNativeDriver: false,
+              }),
+            ]).start();
+            return face;
+          } else {
+            return {
+              ...i,
+              animatedXY: new Animated.ValueXY(xy),
+              animatdWH: new Animated.ValueXY(wh),
+            };
+          }
+        });
+      });
+    },
+    [windowDimensions],
+  );
 
   const frameProcessor = useFrameProcessor(frame => {
     'worklet';
-    console.info(FrameProcessorPlugins);
+
+    const scannedFaces = FrameProcessorPlugins.scanFaces(frame) as Face[];
+    handleScan({width: frame.width, height: frame.height}, scannedFaces);
   }, []);
 
   return (
@@ -32,8 +91,107 @@ function App(): JSX.Element {
           style={{flex: 1}}
         />
       )}
+      <View style={StyleSheet.absoluteFill}>
+        {trackedFaces.map(i => {
+          return (
+            <Animated.View
+              key={i.trackingId}
+              style={[
+                styles.boundingBox,
+                {
+                  top: i.animatedXY.y,
+                  left: i.animatedXY.x,
+                  width: i.animatdWH.x,
+                  height: i.animatdWH.y,
+                },
+              ]}
+            />
+          );
+        })}
+      </View>
     </View>
   );
 }
 
 export default App;
+
+type Vec2D = {
+  x: number;
+  y: number;
+};
+
+type Face = {
+  rollAngle: number;
+  pitchAngle: number;
+  yawAngle: number;
+  bounds: {
+    top: number;
+    left: number;
+    height: number;
+    width: number;
+  };
+  trackingId?: number;
+};
+
+type Dimensions = {width: number; height: number};
+
+const styles = StyleSheet.create({
+  boundingBox: {
+    position: 'absolute',
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+});
+const useRunInJSCallback = <C extends ContextType, T, A extends Array<unknown>>(
+  fn: (this: C, ...args: A) => T,
+  deps: unknown[],
+): ((...args: A) => Promise<T>) =>
+  useMemo(() => Worklets.createRunInJsFn(fn), deps);
+
+const ajdustToView = (
+  frameDimensions: Dimensions,
+  viewDimensions: Dimensions,
+) => {
+  'worklet';
+  const {width: viewWidth, height: viewHeight} = viewDimensions;
+
+  const aspectRatio = viewWidth / viewHeight;
+
+  const frameWidth = frameDimensions.width;
+  const frameHeight = frameDimensions.height;
+
+  const frameAspectRatio = frameWidth / frameHeight;
+
+  let widthRatio: number;
+  let heightRatio: number;
+  let offsetX = 0;
+  let offsetY = 0;
+  if (frameAspectRatio < aspectRatio) {
+    widthRatio = viewWidth / frameWidth;
+    const croppedFrameHeight = frameWidth / aspectRatio;
+    offsetY = (frameHeight - croppedFrameHeight) / 2;
+    heightRatio = viewHeight / croppedFrameHeight;
+  } else {
+    heightRatio = viewHeight / frameHeight;
+    const croppedFrameWidth = aspectRatio * frameHeight;
+    offsetX = (frameWidth - croppedFrameWidth) / 2;
+    widthRatio = viewWidth / croppedFrameWidth;
+  }
+
+  return {
+    adjustPoint: (point: Vec2D): Vec2D => {
+      let y = (point.y - offsetY) * heightRatio;
+
+      return {
+        x: (point.x - offsetX) * widthRatio,
+        y,
+      };
+    },
+    adjustSize: (size: Vec2D): Vec2D => {
+      return {
+        x: size.x * widthRatio,
+        y: size.y * heightRatio,
+      };
+    },
+  };
+};
---
+++ ios/FrameProcessors/FaceDetector/FaceDetectorPlugin.swift
@@ -0,0 +1,134 @@
+import Vision
+import MLKitFaceDetection
+import MLKitVision
+import CoreML
+
+import UIKit
+import AVFoundation
+
+@objc
+public class FaceDetectorPlugin : FrameProcessorPlugin {
+
+    override public func name() -> String! {
+      return "scanFaces"
+    }
+
+    static var FaceDetectorOption: FaceDetectorOptions = {
+        let option = FaceDetectorOptions()
+        option.contourMode = .none
+        option.classificationMode = .none
+        option.landmarkMode = .none
+        option.performanceMode = .accurate // doesn't work in fast mode!, why?
+        option.isTrackingEnabled = true
+        return option
+    }()
+
+    static var faceDetector = FaceDetector.faceDetector(options: FaceDetectorOption)
+
+    private static func processContours(from face: Face) -> [String:[[String:CGFloat]]] {
+      let faceContoursTypes = [
+        FaceContourType.face,
+        FaceContourType.leftEyebrowTop,
+        FaceContourType.leftEyebrowBottom,
+        FaceContourType.rightEyebrowTop,
+        FaceContourType.rightEyebrowBottom,
+        FaceContourType.leftEye,
+        FaceContourType.rightEye,
+        FaceContourType.upperLipTop,
+        FaceContourType.upperLipBottom,
+        FaceContourType.lowerLipTop,
+        FaceContourType.lowerLipBottom,
+        FaceContourType.noseBridge,
+        FaceContourType.noseBottom,
+        FaceContourType.leftCheek,
+        FaceContourType.rightCheek,
+      ]
+
+      let faceContoursTypesStrings = [
+        "FACE",
+        "LEFT_EYEBROW_TOP",
+        "LEFT_EYEBROW_BOTTOM",
+        "RIGHT_EYEBROW_TOP",
+        "RIGHT_EYEBROW_BOTTOM",
+        "LEFT_EYE",
+        "RIGHT_EYE",
+        "UPPER_LIP_TOP",
+        "UPPER_LIP_BOTTOM",
+        "LOWER_LIP_TOP",
+        "LOWER_LIP_BOTTOM",
+        "NOSE_BRIDGE",
+        "NOSE_BOTTOM",
+        "LEFT_CHEEK",
+        "RIGHT_CHEEK",
+      ];
+
+      var faceContoursTypesMap: [String:[[String:CGFloat]]] = [:]
+
+      for i in 0..<faceContoursTypes.count {
+        let contour = face.contour(ofType: faceContoursTypes[i]);
+
+        var pointsArray: [[String:CGFloat]] = []
+
+        if let points = contour?.points {
+          for point in points {
+            let currentPointsMap = [
+                "x": point.x,
+                "y": point.y,
+            ]
+
+            pointsArray.append(currentPointsMap)
+          }
+
+          faceContoursTypesMap[faceContoursTypesStrings[i]] = pointsArray
+        }
+      }
+
+      return faceContoursTypesMap
+    }
+
+    private static func processBoundingBox(from face: Face) -> [String:Any] {
+        let frameRect = face.frame
+
+        return [
+          "left": frameRect.origin.x,
+          "top": frameRect.origin.y,
+          "width": frameRect.width,
+          "height": frameRect.height,
+          "boundingCenterX": frameRect.midX,
+          "boundingCenterY": frameRect.midY
+        ]
+    }
+
+    public override func callback(_ frame: Frame!, withArguments arguments: [Any]!) -> Any! {
+        let image = VisionImage(buffer: frame.buffer)
+        image.orientation = .up
+
+        var faceAttributes: [Any] = []
+
+        do {
+            let faces: [Face] =  try FaceDetectorPlugin.faceDetector.results(in: image)
+            if (!faces.isEmpty){
+                for face in faces {
+                    var map: [String: Any] = [:]
+
+                    map["rollAngle"] = face.headEulerAngleZ  // Head is tilted sideways rotZ degrees
+                    map["pitchAngle"] = face.headEulerAngleX  // Head is rotated to the uptoward rotX degrees
+                    map["yawAngle"] = face.headEulerAngleY   // Head is rotated to the right rotY degrees
+                    map["leftEyeOpenProbability"] = face.leftEyeOpenProbability
+                    map["rightEyeOpenProbability"] = face.rightEyeOpenProbability
+                    map["smilingProbability"] = face.smilingProbability
+                    map["bounds"] = FaceDetectorPlugin.processBoundingBox(from: face)
+                    // map["contours"] = processContours(from: face)
+                    if(face.hasTrackingID) {
+                        map["trackingId"] = face.trackingID
+                    }
+
+                    faceAttributes.append(map)
+                }
+            }
+        } catch _ {
+            return nil
+        }
+        return faceAttributes
+    }
+}
